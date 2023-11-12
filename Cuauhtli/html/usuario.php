de5e670dff5ae4df4e24d022b64862a376d07bde
RaulChaires <raul.hernandez.chaires@estudiante.uacm.edu.mx>
2023-11-11 21:19:39 -0600

    Se añade nuevo commit modificado

---
+++ Cuauhtli/html/usuario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../imagenes/uacm.jpg" type="image/x-icon" />
+    <link rel="stylesheet" type="text/CSS" href="../CSS/style.css" />
+    <title>CUAUHTLI</title>
+</head>
+<body>
+   
+   <div id="form">
+    <form action="../PHP/validar.php" method="post">
+        <h1>Bienvenido amig@ UACEMITA:</h1>
+        <p>Usuario<input type="text" placeholder="ingresa tu correo" name="usuario" required></p>
+        <p>Contraseña<input type="password" placeholder="ingresa tu contraseña" name="contraseña" required></p>
+        <input type="submit" value="ingresar">
+    </form>
+</body>
+   </div>
+  
+</body>
+</html>
