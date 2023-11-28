352318f74a50ca14c408194c6dfa823023d68114
Evama22 <144073076+Evama22@users.noreply.github.com>
2023-11-27 19:26:55 -0600

    Add files via upload
    Se anexa nuevamente archivo ya que se le realizaron modificaciones

---
+++ Cuauhtli/PHP/proceso_guardar.php
@@ -0,0 +1,18 @@
+<?php
+
+    include("conexion.php");
+
+    $nombre = $_POST['nombre'];
+    $imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+
+    $query = "INSERT INTO tabla_imagen(nombre,imagen) VALUES('$nombre','$imagen')";
+    $resultado = $conexion->query($query);
+
+    if($resultado){
+            //echo"Si se inserto";
+        header ("Location: mostrar.php");
+    }
+        else{
+            echo "No se inserto";
+        }
+?>
